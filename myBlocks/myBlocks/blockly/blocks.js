9f66317d0d3151b00488c010a2f9002907b08066
Srshien <74400607+Srshien@users.noreply.github.com>
2020-11-13 16:05:47 +0800

    Add files via upload

---
+++ myBlocks/myBlocks/blockly/blocks.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['sayhello'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("ni hao");
+    this.setPreviousStatement(true, null);
+    this.setColour(315);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ myBlocks/myBlocks/blockly/javascript.js
@@ -0,0 +1,5 @@
+Blockly.JavaScript['sayhello'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("ni hao")';
+  return code;
+};
